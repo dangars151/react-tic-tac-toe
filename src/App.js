072f01abe405583cf945240b28a7256e58970dce
My Name <dangars151@gmail.com>
2024-04-06 00:32:40 +0700

    feat: show current move

--- src/App.js
+++ src/App.js
@@ -70,11 +70,21 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+
+    if (move > 0 && move == currentMove) {
+      return (
+        <li key={move}>
+          <b onClick={() => jumpTo(move)}>{"You are at move #" + move}</b>
+        </li>
+      );
+    }
+
     if (move > 0) {
       description = 'Go to move #' + move;
     } else {
       description = 'Go to game start';
     }
+
     return (
       <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
